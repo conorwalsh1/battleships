b7900e436997aa800c53aefe6ceb80428def8e52
conorwalsh1 <conorwalsh7@gmail.com>
2021-09-13 15:33:53 +0000

    Add function to print player and computer boards to terminal

--- run.py
+++ run.py
@@ -2,20 +2,41 @@ from random import randint
 
 class Board:
 
-    def __init__(self, num_ships, player_name, board_size, turn):
+    def __init__(self, num_ships, player_name, board_size, type):
         self.num_ships = num_ships
+        self.board = board
         self.player_name = player_name
         self.board_size = board_size
-        self.turn = turn
+        self.type = type
         self.ships = []
         self.guesses = []
+
+    board = []    
+
+    for _ in range(5):
+        board.append(["0"] * 5)
+
+    def print_board(board):
+        for row in board:
+            print(" ".join(row))
+
+# def populate_board()
+
+# def computer_guess()
     
-def start_game():
+# def start_game()
 
     num_ships = 4
     board_size = 5
     print("Welcome to Battleships. Are you ready to go to war? Enlist below if you are.")
     player_name = input("Enter your name soldier: \n")
-    print(f"Glad to have you aboard {player_name}. The board size is {board_size} x {board_size}, you have {num_ships} ships to protect and {num_ships} ships to eliminate.\nBest of luck comrade, you'll need it out there.")
+    print(f"Glad to have you aboard {player_name}. The board size is {board_size} x {board_size}, you have {num_ships} ships to protect and {num_ships} ships to eliminate.\nBest of luck comrade, you're going to need it out there.")
+    print("+" * 35)
+    print(f"{player_name}'s Board:")
+    print_board(board)
+    print("Computer's Board:")
+    print_board(board)
+    print("+" * 35)
+    
 
-start_game()
+# start_game()
